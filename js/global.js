0fccdf32f37ec65caf2e14fd086fd630f3936aa5
Nickolay <Nickolay@Thecirclenetwork.eu>
2024-09-07 22:52:50 +0200

    Fixed mobile Nav media sizing issue & misc css

--- js/global.js
+++ js/global.js
@@ -1,26 +1,24 @@
-const mq = window.matchMedia("(max-width: 600px)");
+const mq = window.matchMedia("(max-width: 800px)");
 
 if (mq.matches) {
     const mobileNavIcon = document.getElementById("mobileNavMenuButton");
     const mobileDropdownMenu = document.getElementById("mobileDropMenu");
+    console.log('0');
+    
 
     mobileNavIcon.addEventListener("click", function() {
         if (mobileDropdownMenu.style.display != "block") {
+            console.log('1');
+            
 
             mobileDropdownMenu.style.display = "block";
 
         } else if (mobileDropdownMenu.style.display = "block") {
+            console.log('2');
+            
 
             mobileDropdownMenu.style.display = "none";
             
         }
     })
 }
-
-const accountBtn1 = document.getElementById("btn1");
-const accountBtn2 = document.getElementById("btn2");
-const accountBtn3 = document.getElementById("btn3");
-
-accountBtn2.addEventListener("click", function() {
-    //
-})
